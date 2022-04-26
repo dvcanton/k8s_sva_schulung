42d9ba31f22fae463cef0611ebcba5b5c5fb1709
Dave Canton <dave.canton@sva.de>
2022-04-26 17:01:37 +0200

    fixes

--- vue-app/build.sh
+++ vue-app/build.sh
@@ -1,6 +1,7 @@
 
 npm install
 
-sudo docker build -t dvcanton/wordle-generator-app:0.0.1 .
-# docker run -it -p 8081:8080 -d --name generator-app dvcanton/wordle-generator-app:0.0.1
-# docker push dvcanton/wordle-generator-app:0.0.1
+sudo docker build -t dvcanton/wordle-generator-app:latest .
+sudo docker push dvcanton/wordle-generator-app:latest
+
+# sudo docker run -it -p 8081:8080 -d --name generator-app dvcanton/wordle-generator-app:lastest
---
+++ vue-app/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ROOT_DIR=/usr/share/nginx/html
+
+# Replacing env constants in JS
+for file in src/components/*.vue* ;
+do
+  sed -i 's|BACKEND_SERVER|'${BACKEND_SERVER}'|g' $file
+  sed -i 's|BACKEND_PORT|'${BACKEND_PORT}'|g' $file
+done
