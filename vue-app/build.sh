6447b6370afc3a93cd2a2fb6a04d3b6408959eca
Dave Canton <dave.canton@sva.de>
2022-04-26 17:10:15 +0200

    test

--- vue-app/build.sh
+++ vue-app/build.sh
@@ -4,4 +4,4 @@ npm install
 sudo docker build -t dvcanton/wordle-generator-app:latest .
 sudo docker push dvcanton/wordle-generator-app:latest
 
-# sudo docker run -it -p 8081:8080 -d --name generator-app dvcanton/wordle-generator-app:lastest
+# sudo docker run -it -p 3000:3000 -e BACKEND_SERVER=http://localhost -e BACKEND_PORT=9090 -d --name generator-app dvcanton/wordle-generator-app:latest
