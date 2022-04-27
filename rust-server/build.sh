8186f84fdcbc39022307ea2c26d3991e9c574825
Dave Canton <dave.canton@sva.de>
2022-04-27 07:23:19 +0200

    fixes

--- rust-server/build.sh
+++ rust-server/build.sh
@@ -4,7 +4,7 @@ set -ex
 cargo build --release
 cp target/release/rust_app ./app
 sudo docker build -t dvcanton/wordle-generator-server:latest .
-sudo docker push dvcanton/wordle-generator-app:latest
+sudo docker push dvcanton/wordle-generator-server:latest
 
 # sudo sudo docker run -it -p 9090:9090 -d --name generator-server dvcanton/wordle-generator-server:latest
 # sudo sudo docker run -p 4433:4433 -e ENABLE_SERVER_SSL=true -e CERT_PATH=/files/server_cert.pem -e  KEY_PATH=/files/server_key.pem -d --name generator-server dvcanton/wordle-generator-server:latest
